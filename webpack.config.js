77eb21b4deb11fa96e97de94b29f58306df53e87
Bernardus Billy Tjiptoning <8053504+bernardusbilly@users.noreply.github.com>
2018-05-13 15:51:36 -0700

    first app set up

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  entry: __dirname + '/src/js/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/dist'
+  }
+};
